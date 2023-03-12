ae825f46d43f7a53564f39dc1e2f5ea9ac9957d9
NadiaJoy <iminneverwhere@gmail.com>
2023-03-12 20:49:58 +0200

    1. maven test command fixed 2. results file added

--- src/main/java/org/example/Insurance.java
+++ src/main/java/org/example/Insurance.java
@@ -1,2 +1,18 @@
-package org.example;public class Insurance {
+package org.example;
+
+public class Insurance {
+
+    public int CalcInsurance(int age) {
+        int result;
+
+        if (age >= 30 && age <= 65) {
+            result = 100;
+        } else if (age >= 18 && age <= 29) {
+            result = 120;
+        } else if (age >= 66 && age <= 79)
+            result = 130;
+        else
+            result = 0;
+        return result;
+    }
 }
--- src/test/java/org/example/InsuranceTest.java
+++ src/test/java/org/example/InsuranceTest.java
@@ -1,2 +1,47 @@
-package org.example;public class InsuranceTest {
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InsuranceTest {
+    Insurance insurance;
+
+    @BeforeEach
+    public void setBeforeTest () {
+        insurance = new Insurance();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {30, 31, 64, 65, 40})
+    public void minInsuranceTest (int age) {
+        int result;
+        result = insurance.CalcInsurance(age);
+        Assert.assertEquals(result, 100);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {18, 19, 28, 29, 25})
+    public void mediumInsuranceTest (int age) {
+        int result;
+        result = insurance.CalcInsurance(age);
+        Assert.assertEquals(result, 120);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {66, 67, 78, 79, 70})
+    public void maxInsuranceTest (int age) {
+        int result;
+        result = insurance.CalcInsurance(age);
+        Assert.assertEquals(result, 130);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {17, 80, 0, 1, -1})
+    public void nonInsuranceTest (int age) {
+        int result;
+        result = insurance.CalcInsurance(age);
+        Assert.assertEquals(result, 0);
+    }
 }
